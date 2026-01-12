9198680264dcc2bec40d17e6a5d80b68d18b07d7
icemedia001 <teniopemipo@gmail.com>
2026-01-12 13:49:38 +0100

    chore: update app routing and global styles

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,38 @@ import { GalleryProvider } from './context/GalleryContext';
 import Cart from './components/Cart';
 import ClientLogin from './pages/ClientLogin';
 import ClientGallery from './pages/ClientGallery';
+
+import ServiceDetail from './pages/ServiceDetail';
+import BookingCart from './pages/BookingCart';
+import UserAuth from './pages/UserAuth';
+import UserDashboard from './pages/UserDashboard';
+import { BookingProvider } from './context/BookingContext';
+import { AuthProvider } from './context/AuthContext';
 import './App.css';
 
 function App() {
   return (
     <StoreProvider>
       <GalleryProvider>
-        <Router>
-          <Cart />
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/store" element={<Store />} />
-            <Route path="/admin" element={<Admin />} />
-            <Route path="/gallery" element={<ClientLogin />} />
-            <Route path="/gallery/:code" element={<ClientGallery />} />
-          </Routes>
-        </Router>
+        <AuthProvider>
+          <BookingProvider>
+            <Router>
+              <Cart />
+              <Routes>
+                <Route path="/" element={<Landing />} />
+                <Route path="/portfolio" element={<Portfolio />} />
+                <Route path="/store" element={<Store />} />
+                <Route path="/services/:id" element={<ServiceDetail />} />
+                <Route path="/booking" element={<BookingCart />} />
+                <Route path="/login" element={<UserAuth />} />
+                <Route path="/dashboard" element={<UserDashboard />} />
+                <Route path="/admin" element={<Admin />} />
+                <Route path="/gallery" element={<ClientLogin />} />
+                <Route path="/gallery/:code" element={<ClientGallery />} />
+              </Routes>
+            </Router>
+          </BookingProvider>
+        </AuthProvider>
       </GalleryProvider>
     </StoreProvider>
   );
